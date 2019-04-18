05c4ad94759443677563fdd0db8cd542719b7044
Joy Hou <544088192@qq.com>
2019-04-18 22:54:36 +0800

    [FEA] 常用的样例

--- syntax.mk
+++ syntax.mk
@@ -5,6 +5,15 @@ all:
 define INFO
 	-@echo  "\033[32m install $(1)  ... \033[0m"
 endef
+
+SUBDIRS=src example
+# 用ifnef来判断变量是否定义
+ifndef TOP_DIR
+	# 用shell函数获取绝对路径
+	TOP_DIR = $(dir $(shell pwd)/../)
+endif
+# 用include来插入其它mk文件
+-include env.mk
 #compile
 #
 #-Wl,-rpath选项则是指定运行编译时ld的搜索路径,指定后即可以用ldd a.out看见各个共享库的路径用readelf -d a.out可以看到多了一个（RPATH）的选项，即是共享动态库的路径
@@ -12,5 +21,38 @@ endef
 #
 #1. 编译.so时若要将其他.a编译进去不能使用-l进行链接，这是无效的，只有编译执行程序时才是有效的
 install:
-	$(call INFO, .vimrc)
-	[ -f ~/.vimrc ] && mv ~/.vimrc ~/.vimrc.back
+	echo $(TOP_DIR)
+	# $(call) 和 $()均可以直接调用函数
+	$(call, INFO .vimrc)
+	$(INFO .vimrc)
+	# 如下是shell中的for语句 用$()和$$来区别shell变量和makefile变量
+	for d in $(SUBDIRS); do [ -d $$d ] && $(MAKE) -C $$d; done
+	[ -f ~/ccc ] && mv ~/aaa ~/vvv
+
+main: main.o
+	gcc $< -o $@ -L./ -ltest  #-L指定目录，-l指定库名字
+
+libtest.so (a.o b.o) : a.o b.o #.so和后面的'('之间一定要有空格
+
+	@echo '$$% = '$% #仅当目标是函数库文件中,表示规则中的目标成员名 扩展时只有一个文件
+
+	@echo '$$@ = '$@ #规则中的目标文件集 扩展时只有一个文件 这个和'$^'我们常用
+
+	@echo '$$? = '$? #所有比目标新的依赖目标的集合(哪些依赖文件发生改变则 $?会变成改变文件的集合)   扩展时一个文件名列表
+
+	@echo '$$* = '$* #                        扩展时只有一个文件
+
+	@echo '$$+ = '$+ #  也是所有依赖目标的集合。只是它不去除重复的依赖目标         扩展时一个文件名列表
+
+	@echo '$$^ = '$^ #  所有的依赖目标的集合            扩展时一个文件名列表
+
+	@echo '$$< = '$< #依赖目标中的第一个目标名字 扩展时只有一个文件
+
+	ar cr $@ $?
+
+%.o:%.c 
+	gcc -c  $<  -o $@
+# 打so.和.a参考libgpio的工程
+.PHONY:clean
+clean:
+	-rm *.o testlib *.so
