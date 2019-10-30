481ceeca121c48b1cb8d066ed73804380e779af7
Joy Hou <544088192@qq.com>
2019-10-30 23:31:36 +0800

    just style

--- template1/env.mk
+++ template1/env.mk
@@ -1,11 +1,11 @@
 #CROSS_COMPILE=/opt/buildroot-master/output/host/usr/bin/arm-linux-
 CC=$(CROSS_COMPILE)gcc
 AR=$(CROSS_COMPILE)ar
-INC=-I$(TOP_DIR)/libgpio
 # 等价于config.h -->　#define MAX_NUM 12
 # 等价于config.h -->　#define USE_A
-CFLAGS := -D MAX_NUM=12 
+CFLAGS += -D MAX_NUM=12 
 CFLAGS += -D USE_A 
+CFLAGS += -I$(TOP_DIR)/include
 #CFLAGS += --define-ABC   ABC=string #该句语法错误
 V := 1
 ifeq ($(V), 1)
