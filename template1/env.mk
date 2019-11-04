62fbe4087962ef142eb86453303aeaaeb58e3d17
houwentaoff <544088192@qq.com>
2019-11-04 10:57:15 +0800

    Update env.mk

--- template1/env.mk
+++ template1/env.mk
@@ -1,10 +1,17 @@
+
 #CROSS_COMPILE=/opt/buildroot-master/output/host/usr/bin/arm-linux-
 CC=$(CROSS_COMPILE)gcc
 AR=$(CROSS_COMPILE)ar
+ARCH=arm
 # 等价于config.h -->　#define MAX_NUM 12
 # 等价于config.h -->　#define USE_A
 CFLAGS += -D MAX_NUM=12 
-CFLAGS += -D USE_A 
+CFLAGS += -D DEBUG 
+CFLAGS += -D RTE_ARCH_64
+CFLAGS += -D RTE_FORCE_INTRINSICS=y
+
+CFLAGS += -g
+
 CFLAGS += -I$(TOP_DIR)/include
 #CFLAGS += --define-ABC   ABC=string #该句语法错误
 V := 1
